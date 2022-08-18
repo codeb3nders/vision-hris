2925885a3b8914a47de5e55b4f3b0e755d0f0e30
Edmar Dausan <edmardausan15@gmail.com>
2022-08-19 02:43:54 +0800

    View List of Employees with filter 1. Search Bar added

--- src/components/HRDashboard/EmployeeDatabase.tsx
+++ src/components/HRDashboard/EmployeeDatabase.tsx
@@ -1,8 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Card, Button, Link } from '@mui/material';
-import { AddCircleOutlineTwoTone, UploadTwoTone } from '@mui/icons-material';
+import {
+  Card,
+  Button,
+  Avatar,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Chip,
+} from '@mui/material';
+import {
+  AddCircleOutlineTwoTone,
+  Close,
+  EmailTwoTone,
+  PhoneIphoneTwoTone,
+  Search,
+  UploadTwoTone,
+} from '@mui/icons-material';
 import NewEmployeeProfile from './new.employee.profile';
 import { getEmployeesEndpoint } from 'apis/employees';
 import { EmployeeI } from 'slices/interfaces/employeeI';
@@ -13,6 +30,8 @@ import { MainCtx } from 'components/Main';
 type Props = {};
 
 export const EmployeeCtx = createContext<any>({});
+
+type DisplayPhotosI = { employeeNo: string; photo: string };
 
 const EmployeeDatabase: React.FC<Props> = () => {
   const location = useLocation();
@@ -28,9 +47,13 @@ const EmployeeDatabase: React.FC<Props> = () => {
   const [refresh, setRefresh] = useState<boolean>(false);
 
   const [employees, setEmployees] = useState<EmployeeI[]>([]);
+  const [tempEmployees, setTempEmployees] = useState<EmployeeI[]>([]);
   const [open, setOpen] = useState(false);
+  const [displayPhotos, setDisplayPhotos] = useState<DisplayPhotosI[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
 
   useEffect(() => {
+    handleGetDisplayPhoto();
     if (location.pathname === '/people/employees') {
       setIsTable(true);
     } else {
@@ -39,7 +62,7 @@ const EmployeeDatabase: React.FC<Props> = () => {
   }, [location]);
 
   useEffect(() => {
-    employees && employees?.length <= 0 && getEmployees();
+    employees && employees?.length <= 0 && !searchText && getEmployees();
   }, [employees]);
 
   useEffect(() => {
@@ -49,18 +72,78 @@ const EmployeeDatabase: React.FC<Props> = () => {
     }
   }, [refresh]);
 
+  const handleGetDisplayPhoto = () => {
+    const local_dps: any = localStorage.getItem('display_photos');
+    const dps = JSON.parse(local_dps);
+
+    setDisplayPhotos(dps);
+  };
+
   const getEmployees = async () => {
     try {
       const res = await getEmployeesEndpoint();
-      setEmployees(
-        res.data?.map((r: any) => {
-          return { ...r, id: r.employeeNo };
-        })
-      );
+      const employees = res.data?.map((r: any) => {
+        return { ...r, id: r.employeeNo };
+      });
+      setTempEmployees(employees);
+      setEmployees(employees);
     } catch (error: any) {
       console.log(error.message);
     }
   };
+
+  const handleSearch = () => {
+    if (searchText) {
+      const search_result = tempEmployees.filter((employee: EmployeeI) => {
+        const active = employee.isActive ? 'Active' : 'Inactive';
+        const text = searchText?.toLowerCase();
+        const employeeNo = employee?.employeeNo?.toLowerCase();
+        const email = employee?.personalEmail?.toLowerCase();
+        const phone = employee?.personalContactNumber?.toLowerCase();
+        const lastName = employee?.lastName?.toLowerCase();
+        const firstName = employee?.firstName?.toLowerCase();
+        const position = employee?.position?.toLowerCase();
+        const rank = employee?.rank?.toLowerCase();
+        const department = employee?.department?.toLowerCase();
+        const location = employee?.location?.toLowerCase();
+        const employmentStatus = employee?.employmentStatus?.toLowerCase();
+        const reportsTo = employee?.reportsTo?.toLowerCase();
+        const isActive = active?.toString()?.toLowerCase();
+
+        return (
+          employeeNo?.includes(text) ||
+          email?.includes(text) ||
+          phone?.includes(text) ||
+          lastName?.includes(text) ||
+          firstName?.includes(text) ||
+          position?.includes(text) ||
+          rank?.includes(text) ||
+          department?.includes(text) ||
+          employmentStatus?.includes(text) ||
+          reportsTo?.includes(text) ||
+          isActive?.includes(text) ||
+          location?.includes(text)
+        );
+      });
+
+      setEmployees(search_result);
+    } else {
+      setEmployees(tempEmployees);
+    }
+  };
+
+  const handleSearchOnEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    if (!searchText) {
+      setEmployees(tempEmployees);
+    }
+  }, [searchText]);
 
   return (
     <EmployeeCtx.Provider value={{ setRefresh }}>
@@ -71,51 +154,110 @@ const EmployeeDatabase: React.FC<Props> = () => {
       />
 
       <Card sx={{ mt: 5, p: 2 }}>
-        <div style={{ marginBottom: 16, textAlign: 'right' }}>
-          <Button startIcon={<UploadTwoTone />} sx={{ mr: 1 }}>
-            Upload Employee Details
-          </Button>
-          <Button
-            startIcon={<AddCircleOutlineTwoTone />}
-            onClick={() => setOpen(true)}
-          >
-            Add New Employee
-          </Button>
-        </div>
+        <section className='flex flex-row mb-4 items-center'>
+          <div className='w-1/2'>
+            <FormControl variant='outlined' fullWidth size='small'>
+              <InputLabel htmlFor='search-employee'>Search Employee</InputLabel>
+              <OutlinedInput
+                fullWidth
+                value={searchText}
+                onKeyUp={handleSearchOnEnter}
+                id='search-employee'
+                onChange={(e: any) => setSearchText(e.target.value)}
+                endAdornment={
+                  <InputAdornment position='end'>
+                    <IconButton
+                      className={searchText ? 'flex' : 'hidden'}
+                      aria-label='search employee'
+                      edge='end'
+                      onClick={() => {
+                        setEmployees(tempEmployees);
+                        setSearchText('');
+                      }}
+                    >
+                      <Close fontSize='small' />
+                    </IconButton>
+                    <IconButton
+                      aria-label='search employee'
+                      edge='end'
+                      onClick={handleSearch}
+                    >
+                      <Search fontSize='small' />
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label='Search Employee'
+              />
+            </FormControl>
+          </div>
+          <div className='w-1/2 flex flex-row gap-2 items-center justify-end'>
+            <Button startIcon={<UploadTwoTone />} sx={{ mr: 1 }}>
+              Upload Employee Details
+            </Button>
+            <Button
+              startIcon={<AddCircleOutlineTwoTone />}
+              onClick={() => setOpen(true)}
+            >
+              Add New Employee
+            </Button>
+          </div>
+        </section>
 
-        <div style={{ height: 400, width: '100%' }}>
+        <section style={{ height: 400, width: '100%' }}>
           <DataGrid
             disableSelectionOnClick
             rows={employees || []}
-            columns={columns(setViewDetails)}
+            columns={columns(setViewDetails, displayPhotos)}
             pageSize={5}
             rowsPerPageOptions={[5]}
             checkboxSelection
-            loading={employees?.length <= 0}
+            loading={employees?.length <= 0 && !searchText}
+            autoHeight
           />
-        </div>
+        </section>
       </Card>
     </EmployeeCtx.Provider>
   );
 };
 
-const columns = (setViewDetails: any) => [
+const columns = (setViewDetails: any, displayPhotos: DisplayPhotosI[]) => [
   {
-    field: 'employee_name',
-    headerName: 'Employee name',
-    width: 300,
-    renderCell: (cell) => {
+    field: 'personalEmail',
+    headerName: 'Email & Viber Number',
+    width: 270,
+    renderCell: (cell: any) => {
+      const employee_dp = displayPhotos.filter(
+        (dp) => dp.employeeNo === cell.row.employeeNo
+      )[0];
+
       return (
-        <Link
-          underline='none'
-          variant='button'
-          style={{ cursor: 'pointer' }}
+        <button
+          className='p-1 border-0 outline-none m-0 hover:bg-gray-200 w-full rounded-sm'
           onClick={() => setViewDetails({ details: cell.row, status: true })}
         >
-          <div className='whitespace-normal'>
-            {cell.row.lastName}, {cell.row.firstName} {cell.row.middleName}
+          <div className='flex flex-row gap-2 items-center justify-start'>
+            <div>
+              <Avatar
+                src={employee_dp?.photo}
+                alt={`${cell.row.firstName} ${cell.row.lastName}`}
+                className='w-[36px] h-[36px]'
+              />
+            </div>
+            <div className='whitespace-normal w-full text-start'>
+              <div className='text-sky-500'>
+                <EmailTwoTone fontSize='small' className='text-[12px] mr-1' />
+                {cell.value}
+              </div>
+              <div className='text-xs text-gray-600'>
+                <PhoneIphoneTwoTone
+                  fontSize='small'
+                  className='text-[12px] mr-1'
+                />
+                {cell.row.personalContactNumber}
+              </div>
+            </div>
           </div>
-        </Link>
+        </button>
       );
     },
   },
@@ -128,6 +270,16 @@ const columns = (setViewDetails: any) => [
         {cell.value}
       </span>
     ),
+  },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 250,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 250,
   },
   { field: 'position', headerName: 'Position', width: 200 },
   {
@@ -152,6 +304,25 @@ const columns = (setViewDetails: any) => [
     headerName: 'Employment Status',
     width: 140,
     //   renderCell: (cell) => cell,
+  },
+  {
+    field: 'reportsTo',
+    headerName: 'Reports To',
+    width: 140,
+    //   renderCell: (cell) => cell,
+  },
+  {
+    field: 'isActive',
+    headerName: 'Active',
+    width: 140,
+    renderCell: (cell) => {
+      return (
+        <Chip
+          label={cell.value ? 'Active' : 'Inactive'}
+          color={cell.value ? 'success' : 'default'}
+        />
+      );
+    },
   },
 ];
 
